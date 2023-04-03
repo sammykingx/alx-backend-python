d67aa7ce9c90ac7bba2debda2dd92ee2e28d3837
sammy <sammy@sammykingx.com.ng>
2023-04-03 00:50:56 +0000

    fixed syntax error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''A function that retuns a tuple'''
     return(k, v**2)
 
