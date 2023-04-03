014e6d1aaa0bfe8f8dfb5f7db79d96a226506584
sammy <sammy@sammykingx.com.ng>
2023-04-03 00:27:16 +0000

    fixed check error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,10 +4,10 @@
 '''a module that takes two arguments and returns a tuple'''
 
 
-from typing import Union
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int | float]) -> tuple:
+def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
     '''A function that retuns a tuple'''
     return(k, v**2)
 
