409dd425846fa3db55691aff5813dca2068f3ffa
sammy <sammy@sammykingx.com.ng>
2023-04-04 21:50:03 +0000

    modified file

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -17,8 +17,6 @@ def sum_list(input_list: List[float]) -> float:
 
 
 if __name__ == '__main__':
-    sum_list = __import__('5-sum_list').sum_list
-
     floats = [3.14, 1.11, 2.22]
     floats_sum = sum_list(floats)
     print(floats_sum == sum(floats))
